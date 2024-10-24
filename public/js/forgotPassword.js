3916db4451917dd42877250d0f484ed761798066
Nguyễn Lê Hiếu Nhi <nhinhi7ahs@gmail.com>
2024-10-24 22:26:34 +0700

    add login page, create acc and forgot password of DAIKA TRUC

---
+++ public/js/forgotPassword.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const Back = document.getElementById('back-btn');
+
+    document.addEventListener('click', function() {
+        window.location.href = "/html/login.html";
+    })
+})
